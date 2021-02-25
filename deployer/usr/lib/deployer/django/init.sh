2ae5af3583f2a34616206f7142a2a052b884d4a1
asim3 <asimwebapps@gmail.com>
2021-02-25 22:32:14 +0300

    rewrite error reporting

--- deployer/usr/lib/deployer/django/init.sh
+++ deployer/usr/lib/deployer/django/init.sh
@@ -2,6 +2,11 @@
 
 PROJECT_NAME=$1
 
+
+/usr/lib/deployer/utilities/initiate
+
+# TODO: add .venv inside the project folder
+
 [ -d ".venv" ] || python3 -m venv .venv
 source .venv/bin/activate
 pip install Django \
